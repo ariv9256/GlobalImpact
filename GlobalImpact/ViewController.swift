fb72b0e3565339ee4215cff63ed57e40d17fc0e1
Angela Rivas <snowcats001@gmail.com>
2019-01-10 12:11:57 -0700

    Initial Commit

---
+++ GlobalImpact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlobalImpact
+//
+//  Created by Rivas, Angela on 1/10/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
