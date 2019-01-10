c70687eaa000cdb7bea53687b4b0e448e01c23c1
Angela Rivas <snowcats001@gmail.com>
2019-01-10 13:10:40 -0700

    Added an extension for TimeInterval and an IBAction method linked to the storyboard.

--- GlobalImpact/Controller/ImpactViewController.swift
+++ GlobalImpact/Controller/ImpactViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
-class ImpactViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ImpactViewController: UIViewController
+{
+    @IBOutlet weak var MKMapKit: MKMapView!
+    @IBOutlet weak var MapButton: UIButton!
+    @IBOutlet weak var MapImage: UIImageView!
+    
+    @IBAction func MapButton(_ sender: UIButton)
+    {
+        let impactLocation = CLLocationCoordinate2D(latitude: 20 , longitude: 30)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+        let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+        MKMapKit.animatedZoom(zoomRegion : impactRegion, duration: 2.5)
+    }
+    override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
-
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion: MKCoordinateRegion, duration: TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options:
+            UIView.AnimationOptions.curveEaseIn, animations:
+            {self.setRegion(zoomRegion, animated: true) }, completion: nil)
+    }
+}
